b017e37704ca95182f7153426fc54d5b895c5461
rayauxey <rayauxey@gmail.com>
2024-07-28 16:40:01 +0300

    generalize getStories

--- resolvers/hackernews.ts
+++ resolvers/hackernews.ts
@@ -1,57 +1,64 @@
 type ItemId = number;
 type ItemIdList = ItemId[];
 
-type ItemType = 'job' | 'story' | 'comment' | 'poll' | 'pollopt';
+type ItemType = "job" | "story" | "comment" | "poll" | "pollopt";
 
 export interface Item {
-  id: ItemId;
-  deleted?: boolean;
-  type: ItemType;
-  by: string;
-  time: number;
-  text?: string;
-  dead?: boolean;
-  parent?: number;
-  poll?: number;
-  kids?: ItemIdList;
-  url?: string;
-  score?: number;
-  title: string;
-  parts?: ItemIdList;
-  descendants?: number;
+	id: ItemId;
+	deleted?: boolean;
+	type: ItemType;
+	by: string;
+	time: number;
+	text?: string;
+	dead?: boolean;
+	parent?: number;
+	poll?: number;
+	kids?: ItemIdList;
+	url?: string;
+	score?: number;
+	title: string;
+	parts?: ItemIdList;
+	descendants?: number;
 }
 
 export interface User {
-  id: string;
-  created: number;
-  karma: number;
-  about?: string;
-  submitted: ItemIdList;
+	id: string;
+	created: number;
+	karma: number;
+	about?: string;
+	submitted: ItemIdList;
 }
 
 export interface Story extends Item {
-  type: 'story';
-  descendants: number;
-  score: number;
-  title: string;
-  url: string;
+	type: "story";
+	descendants: number;
+	score: number;
+	title: string;
 }
 
-export const HACKER_NEWS_V0_API = 'https://hacker-news.firebaseio.com/v0';
+const HACKER_NEWS_V0_API = "https://hacker-news.firebaseio.com/v0";
 
-export async function getTopStoriesIds(count: number) : Promise<ItemIdList> {
-  const res = await fetch(`${HACKER_NEWS_V0_API}/topstories.json`);
-  const ids = await res.json() as ItemIdList;
-  return ids.slice(0, count);
+type StoryCategory = "new" | "top" | "best";
+
+export async function getStoriesIds(
+	count: number,
+	category: StoryCategory,
+): Promise<ItemIdList> {
+	const res = await fetch(`${HACKER_NEWS_V0_API}/${category}stories.json`);
+	const ids = (await res.json()) as ItemIdList;
+	return ids.slice(0, count);
 }
 
-export async function getItem<T extends Item>(id: ItemId) : Promise<T> {
-  const res = await fetch(`${HACKER_NEWS_V0_API}/item/${id}.json`);
-  return await res.json() as T;
+export async function getItem<T extends Item>(id: ItemId): Promise<T> {
+	const res = await fetch(`${HACKER_NEWS_V0_API}/item/${id}.json`);
+	return (await res.json()) as T;
 }
 
-export async function getTopStories(count: number) : Promise<Story[]> {
-  const ids = await getTopStoriesIds(count);
-  const stories = await Promise.all(ids.map(id => getItem<Story>(id)));
-  return stories;
+export async function getStories(
+	count: number,
+	category: StoryCategory,
+): Promise<Story[]> {
+	const ids = await getStoriesIds(count, category);
+	const stories = await Promise.all(ids.map((id) => getItem<Story>(id)));
+	return stories;
 }
