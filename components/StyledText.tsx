01c23752f955be127d7da5bc9fccff8f684d3563
rayauxey <rayauxey@gmail.com>
2024-07-28 16:41:34 +0300

    add fonttext component

--- components/StyledText.tsx
+++ components/StyledText.tsx
@@ -1,5 +1,13 @@
-import { Text, TextProps } from './Themed';
+import { Text, type TextProps } from "./Themed";
 
 export function MonoText(props: TextProps) {
-  return <Text {...props} style={[props.style, { fontFamily: 'SpaceMono' }]} />;
+	return <Text {...props} style={[props.style, { fontFamily: "SpaceMono" }]} />;
+}
+
+export function InterText(props: TextProps) {
+	return <Text {...props} style={[props.style, { fontFamily: "Inter" }]} />;
+}
+
+export function FontText(props: TextProps & { font: string }) {
+	return <Text {...props} style={[props.style, { fontFamily: props.font }]} />;
 }
