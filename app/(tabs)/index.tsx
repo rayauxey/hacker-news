fa820115f851c9d7cb6d06895af22ba934401322
rayauxey <rayauxey@gmail.com>
2024-07-28 19:51:54 +0300

    show top stories

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,77 @@
-import { StyleSheet } from 'react-native';
+import {
+	ActivityIndicator,
+	ScrollView,
+	StyleSheet,
+	useColorScheme,
+} from "react-native";
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import { View } from "@/components/Themed";
+import { useQuery } from "@tanstack/react-query";
+import { getStories } from "@/resolvers/hackernews";
+import StoryItem from "@/components/Stories/StoryItem";
+import Colors from "@/constants/Colors";
 
-export default function TabOneScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
-  );
+export default function TopStoriesScreen() {
+	const { data, isLoading } = useQuery({
+		queryKey: ["topStories"],
+		queryFn: async () => getStories(20, "top"),
+	});
+
+	const colorScheme = useColorScheme();
+
+	return (
+		<View style={styles.container}>
+			{isLoading ? (
+				<LoadingSpinner />
+			) : (
+				<ScrollView>
+					{data?.map((story, i) => (
+						<View key={story.id}>
+							<StoryItem story={story} />
+							{i < data.length - 1 && (
+								<View
+									style={{
+										...styles.separator,
+										backgroundColor: colorScheme
+											? Colors[colorScheme].separator
+											: Colors.light.separator,
+									}}
+								/>
+							)}
+						</View>
+					))}
+				</ScrollView>
+			)}
+		</View>
+	);
+}
+
+function LoadingSpinner() {
+	return (
+		<View
+			style={{
+				flex: 1,
+				justifyContent: "center",
+				alignItems: "center",
+			}}
+		>
+			<ActivityIndicator size="large" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+	container: {
+		flex: 1,
+	},
+	title: {
+		fontSize: 20,
+		fontWeight: "bold",
+	},
+	separator: {
+		// marginVertical: 16,
+		height: 1,
+		width: "100%",
+		marginHorizontal: "auto",
+	},
 });
